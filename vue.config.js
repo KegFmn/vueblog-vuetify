88b56579e83009ff7a89670200798d6d3f30d48a
KegFmn <1165624857@qq.com>
2022-04-13 23:51:45 +0800

    修改博客名字和图标

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  publicPath: '/'
+  publicPath: '/', 
+  outputDir: 'dist', // 输出文件目录
+  assetsDir: 'static', // 放置静态资源
+
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= 'MyBlog'
+        return args
+      })
+  },
 }
