7596dede0fa10d432d6d277382a740f11c7026e1
KegFmn <1165624857@qq.com>
2022-04-13 23:54:33 +0800

    新增代码高亮自定义指令

--- src/main.js
+++ src/main.js
@@ -4,21 +4,27 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
-
-import Message from './components/Message/index.js'
+import message from './components/Message/index.js'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github.css'
 
 import "./axios"
 import './permission'
-
 
 Vue.use(mavonEditor)
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
-Vue.prototype.$message = Message
+Vue.prototype.$message = message
 
+Vue.directive('highlight', (el) =>{
+  const blocks = el.querySelectorAll('pre')
+  blocks.forEach((block) => {
+    hljs.highlightBlock(block)
+  })
+})
 
 new Vue({
   router,
