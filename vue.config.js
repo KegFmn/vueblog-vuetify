ee551d1403cf0ad1e7390ea4e83c20c1135f5f64
KegFmn <1165624857@qq.com>
2022-04-14 15:36:40 +0800

    hight.js打包设置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   publicPath: '/', 
   outputDir: 'dist', // 输出文件目录
-  assetsDir: 'static', // 放置静态资源
 
   chainWebpack: config => {
     config
@@ -11,4 +13,19 @@ module.exports = {
         return args
       })
   },
+  
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([{
+          from: 'node_modules/mavon-editor/dist/highlightjs',
+          to: path.resolve(__dirname, '../dist/highlightjs'), // 插件将会把文件导出于/dist/highlightjs之下
+      }, {
+          from: 'node_modules/mavon-editor/dist/markdown',
+          to: path.resolve(__dirname, '../dist/markdown'), // 插件将会把文件导出于/dist/markdown之下
+      }, {
+          from: 'node_modules/mavon-editor/dist/katex', // 插件将会把文件导出
+          to: path.resolve(__dirname, '../dist/katex')
+      }]),
+    ],
+  }
 }
