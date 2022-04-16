6adefc4b5e3db551495fcd203308ee4eeca7bba0
KegFmn <1165624857@qq.com>
2022-04-16 21:32:52 +0800

    完善代码

--- src/components/AppMain/index.vue
+++ src/components/AppMain/index.vue
@@ -7,32 +7,36 @@
 
         <!-- 侧边栏 -->
         <v-col :cols="asideCols" class="left-aside d-none d-lg-flex">
-          <v-sheet rounded="xl" width="100%">
-            <v-list color="transparent" rounded>
-              <v-list-item-group
-                color="primary"
-              >
-                <v-list-item
-                  link
-                  v-for="(type,index) in types"
-                  :key="index"
-                  :to="{name:'Type', params: {typeId: type.id}}"
-                >
-                  <v-list-item-icon>
-                    <v-icon>mdi-bookmark-multiple</v-icon>
-                  </v-list-item-icon>
+          <v-row>
+            <v-col cols="24">
+              <v-sheet rounded="xl" width="100%">
+                <v-list color="transparent" rounded>
+                  <v-list-item-group
+                    color="primary"
+                  >
+                    <v-list-item
+                      link
+                      v-for="(type,index) in types"
+                      :key="index"
+                      :to="{name:'Type', params: {typeId: type.id}}"
+                    >
+                      <v-list-item-icon>
+                        <v-icon>mdi-bookmark-multiple</v-icon>
+                      </v-list-item-icon>
 
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      {{ type.typeName }}
-                    </v-list-item-title>
-                  </v-list-item-content>
+                      <v-list-item-content>
+                        <v-list-item-title>
+                          {{ type.typeName }}
+                        </v-list-item-title>
+                      </v-list-item-content>
 
-                </v-list-item>
-              </v-list-item-group>
+                    </v-list-item>
+                  </v-list-item-group>
 
-            </v-list>
-          </v-sheet>
+                </v-list>
+              </v-sheet>
+            </v-col>
+          </v-row>
         </v-col>
 
         <!-- 内容 -->
@@ -77,15 +81,6 @@ export default {
         case 'xl': return 2
       }
     },
-    asideOffsets() {
-      switch (this.$vuetify.breakpoint.name) {
-        case 'xs': return 0
-        case 'sm': return 0
-        case 'md': return 0
-        case 'lg': return 0
-        case 'xl': return 1
-      }
-    },
     viewCols () {
       switch (this.$vuetify.breakpoint.name) {
         case 'xs': return 12
@@ -106,10 +101,15 @@ export default {
     },
   },
   methods: {
-
+    list() {
+      this.$axios.get('/types').then(res =>{
+        const blogType = res.data.data
+        this.$store.commit('SET_BLOGTYPE', blogType)
+      })
+    }
   },
   mounted() {
-
+    this.list()
   }
 }
 </script>
@@ -117,6 +117,6 @@ export default {
 <style lang="scss" scoped>
 .left-aside{
   position: fixed;
-  margin-left: 7%;
+  left: 7%;
 }
 </style>
