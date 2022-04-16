969fb382666764a1bbde34e99d0aef5d3eebc10b
KegFmn <1165624857@qq.com>
2022-04-16 21:31:42 +0800

    手机Header头部修改为显示主页、留言板、友链、发表博客

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,100 +1,130 @@
 <template>
-  <section>
+  <v-card class="overflow-hidden">
     <!-- 头部组件 -->
     <v-app-bar
       app
       color="white"
+      elevate-on-scroll
     >
       <!-- 抽屉按钮 //仅在小于md以下可见 -->
       <v-app-bar-nav-icon 
         @click="drawer = true"
-        class="d-sm-flex d-md-none"
+        class="d-md-flex d-lg-none"
       ></v-app-bar-nav-icon>
 
-      <!-- 头部内容 -->
-      <v-container class="py-0 fill-height">
+      <v-toolbar-title class="mb-0 font-weight-black text-lg-h5">MyBlog</v-toolbar-title>
 
-        <p class="mb-0 font-weight-black text-lg-h5">MyBlog</p>
-        <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-        <v-btn text link to="/" rounded :class="{ 'mr-5' : !hasLogin}">主页</v-btn>
-        <v-btn text link rounded class="mr-5" v-if="hasLogin" to="/blog/add">发表博客</v-btn>
-
-        <!-- 头像点击悬浮菜单 -->
-        <v-menu
-          min-width="165px"
-          rounded="xl"
-          offset-y
-          nudge-right="300"
-          nudge-bottom="20"
+      <v-row class="d-none d-lg-flex justify-end">
+        <v-btn 
+          text 
+          link
+          rounded
+          v-for="(item, index) in navigations" :key="index" :to="item.to" 
+          v-show="item.name == '发表博客' ? hasLogin : true"
         >
-          
-          <template v-slot:activator="{ on }">
-            <v-btn
-              icon
-              v-on="on"
-              small
+          <v-icon left>
+            {{item.icon}}
+          </v-icon>
+          {{item.name}}
+        </v-btn>
+      </v-row>
+
+      <v-menu
+        min-width="165px"
+        rounded="xl"
+        offset-y
+        nudge-right="300"
+        nudge-bottom="20"
+      >
+        
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            v-on="on"
+            small
+            class="mr-5 ml-5 d-none d-lg-flex"
+          >
+            <v-avatar
+              color="grey darken-1"
+              size="32"  
             >
+              <img :src="user.avatar" alt="Avatar">
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list-item-content class="justify-center">
+            <div class="mx-auto text-center">
               <v-avatar
-                color="grey darken-1"
-                size="32"  
+                color="brown"
               >
                 <img :src="user.avatar" alt="Avatar">
               </v-avatar>
-            </v-btn>
-          </template>
 
-          <v-card>
-            <v-list-item-content class="justify-center">
-              <div class="mx-auto text-center">
-                <v-avatar
-                  color="brown"
-                >
-                  <img :src="user.avatar" alt="Avatar">
-                </v-avatar>
+              <h3 class="mt-2">{{ user.userName }}</h3>
 
-                <h3 class="mt-2">{{ user.userName }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
 
-                <p class="text-caption mt-1">
-                  {{ user.email }}
-                </p>
+              <v-divider class="my-3" v-show="!hasLogin"></v-divider>
+              <v-btn
+                link
+                rounded
+                color="#43A047" 
+                v-show="!hasLogin" 
+                to="/login"
+              >
+                登录
+              </v-btn>
 
-                <v-divider class="my-3" v-show="!hasLogin"></v-divider>
-                <v-btn
-                  link
-                  rounded
-                  type="button"
-                  color="#43A047" 
-                  v-show="!hasLogin" 
-                  to="/login"
-                >
-                  登录
-                </v-btn>
+              <v-divider class="my-3" v-show="hasLogin" ></v-divider>
 
-                <v-divider class="my-3" v-show="hasLogin" ></v-divider>
-                <v-btn
-                  type="button"
-                  rounded 
-                  v-show="hasLogin"
-                  color="#EF5350" 
-                  @click="logout"
-                >
-                  退出
-                </v-btn>
-              </div>
-            </v-list-item-content>
-          </v-card>
-        </v-menu>
-      </v-container>
+              <v-btn
+                rounded 
+                v-show="hasLogin"
+                color="#EF5350" 
+                @click="logout"
+              >
+                退出
+              </v-btn>
+            </div>
+          </v-list-item-content>
+        </v-card>
+      </v-menu>
     </v-app-bar>
 
     <!-- 左边导航抽屉 -->
     <v-navigation-drawer
       app
-      width="206"
+      width="180"
       temporary
       v-model="drawer"
     >
+      <v-list>
+        <v-list-item class="d-flex justify-center">
+          <v-list-item-avatar>
+            <v-img :src="user.avatar"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-content class="py-0">
+            <v-list-item-title class="text-h6 d-flex justify-center">
+              {{user.userName}}
+            </v-list-item-title>
+            <v-list-item-subtitle class="d-flex justify-center">
+              {{user.email}}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
       <v-list shaped>
         <v-list-item-group
           v-model="group"
@@ -102,42 +132,66 @@
         >
           <v-list-item
             link
-            v-for="(type,index) in types"
+            v-for="(item, index) in navigations"
             :key="index"
-            :to="{name:'Type', params: {typeId: type.id}}"
+            :to="item.to"
+            v-show="item.name == '发表博客' ? hasLogin : true"
           >
             <v-list-item-icon>
-              <v-icon>mdi-bookmark-multiple</v-icon>
+              <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-icon>
 
             <v-list-item-content>
               <v-list-item-title>
-                {{ type.typeName }}
+                {{ item.name }}
               </v-list-item-title>
             </v-list-item-content>
-
           </v-list-item>
         </v-list-item-group>
       </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn
+            link
+            block
+            color="#43A047" 
+            v-show="!hasLogin" 
+            to="/login"
+          >
+            登录
+          </v-btn>
+
+          <v-btn
+            block
+            v-show="hasLogin"
+            color="#EF5350" 
+            @click="logout"
+          >
+            退出
+          </v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
-  </section>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: 'Header',
   components: {},
-  computed: {
-    types() {
-      return this.$store.getters.getBlogType
-    }
-  },
   data: () => ({
     user:{
       userName: '请先登录',
       avatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
       email:''
     },
+    navigations: [
+      {to: '/', icon: 'mdi-home-variant', name: '主页'},
+      {to: '', icon: 'mdi-vector-link', name: '友链'},
+      {to: '', icon: 'mdi-calendar-text', name: '留言板'},
+      {to: '/blog/add', icon: 'mdi-pencil', name: '发表博客'},
+    ],
     hasLogin: false,
     drawer: false,
     group: null,
@@ -152,12 +206,6 @@ export default {
         this.$store.commit("REMOVE_INFO")
         this.$router.push("/login")
       })
-    },
-    list() {
-      this.$axios.get('/types').then(res =>{
-        const blogType = res.data.data
-        this.$store.commit('SET_BLOGTYPE', blogType)
-      })
     }
   },
   mounted() {
@@ -167,11 +215,12 @@ export default {
       this.user.email = this.$store.getters.getUser.email
       this.hasLogin = true
     }
-    this.list()
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.v-application--is-ltr .v-list-item__avatar:first-child{
+  margin-right: 0px;
+}
 </style>
