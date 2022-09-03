8823586806b0f495190e15a7a45209539f3ca6b3
KegFmn <1165624857@qq.com>
2022-09-04 02:33:57 +0800

    新增gitignore代码高亮

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
     [
       'prismjs',
       {
-        languages: ['json','html','css','javascript','java','xml','yml','yaml','bash','docker','dockerfile','nginx','git','sql'],
+        languages: ['json','html','css','javascript','java','xml','yml','yaml','bash','docker','dockerfile','nginx','git','gitignore','sql'],
       },
     ],
   ],
