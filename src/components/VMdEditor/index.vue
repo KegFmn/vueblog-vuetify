8c0c803cd3cd399c1ff56dba0929c7efe171598e
KegFmn <1165624857@qq.com>
2022-09-04 22:29:11 +0800

    图片转base64

--- src/components/VMdEditor/index.vue
+++ src/components/VMdEditor/index.vue
@@ -63,16 +63,19 @@ export default {
   },
   methods: {
     handleUploadImage(event, insertImage, files) {
-      // 拿到 files 之后上传到文件服务器，然后向编辑框中插入对应的内容
-      // console.log(files);
-
-      // 此处只做示例
-      // insertImage({
-      //   url: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1269952892,3525182336&fm=26&gp=0.jpg',
-      //   desc: '七龙珠',
-      //   // width: 'auto',
-      //   // height: 'auto',
-      // });
+      for(let i in files) {
+        const reader = new FileReader();
+        reader.readAsDataURL(files[i])
+		    reader.onload = ()=>{
+          const imageBase64 = reader.result
+          insertImage({
+            url: imageBase64,
+            desc: files[i].name,
+            // width: 'auto',
+            // height: 'auto',
+          });
+        }
+      }
     }
   },
 };
