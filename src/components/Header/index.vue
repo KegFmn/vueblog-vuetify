93f126e261a15124bcb2c51528ac7b613c7cac06
KegFmn <1165624857@qq.com>
2022-09-13 22:44:00 +0800

    新增友链界面

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -260,7 +260,7 @@ export default {
   data: () => ({
     user:{
       userName: '请先登录',
-      avatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
+      avatar: '/images/default-avatar.png',
       email:''
     },
     navigations: [
--- src/views/BlogLink.vue
+++ src/views/BlogLink.vue
@@ -1,13 +1,74 @@
 <template>
+  <v-row justify="center">
+    <v-col
+      v-for="(item, i) in items"
+      :key="i"
+      cols="6"
+    >
+      <v-card
+        :color="item.color"
+        :href="item.href"
+        target="_blank"
+        rounded="xl"
+        class="elevation-2 "
+        dark
+      >
+        <div class="d-flex justify-space-between">
+          <div>
+            <v-card-title
+              class="text-h5"
+              v-text="item.title"
+            ></v-card-title>
 
+            <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+          </div>
+
+          <v-avatar
+            class="ma-3"
+            :size="size"
+            rounded="xl"
+          >
+            <v-img :src="item.avatar"></v-img>
+          </v-avatar>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: 'BlogLink',
 
-  data: () => ({
+  computed: {
+    size() {
+      switch(this.$vuetify.breakpoint.name){
+        case 'xs': return 48
+        case 'sm': return 48
+        case 'md': return 125
+        case 'lg': return 125
+        case 'xl': return 125
+      }
+    }
+  },
 
+  data: () => ({
+    items: [
+        {
+          color: '#24292f',
+          href: 'https://github.com/KegFmn',
+          avatar: '/images/github.jpg',
+          title: 'Github',
+          artist: "Let's build from here, together.",
+        },
+        {
+          color: '#fc5531',
+          href: 'https://blog.csdn.net/weixin_47357245?spm=1000.2115.3001.5343',
+          avatar: '/images/csdn.jpg',
+          title: 'CSDN',
+          artist: '专业开发者社区',
+        },
+      ],
   }),
 
   mounted() {
