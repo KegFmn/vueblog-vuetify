d7b40c0beb2724862002a15dc084bcf4d5eca881
KegFmn <48885939+KegFmn@users.noreply.github.com>
2022-09-29 18:18:21 +0800

    设置动态title

--- src/main.js
+++ src/main.js
@@ -29,6 +29,14 @@ Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 Vue.prototype.$message = Message
 
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
 new Vue({
   router,
   store,
