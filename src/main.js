dff10a42065281e4b49f47073c7a3b08f94408fa
KegFmn <1165624857@qq.com>
2023-06-17 15:36:29 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -18,35 +18,56 @@ const options = {
 	fonts:{
     extendedJsFonts:true
   },
-	excludes:{
-		language: true,
-		colorDepth: true
-	}	
-}
-Fingerprint2.get({options},(components) => {
-  const values = components.map((component,index) => {
-    //把微信浏览器里UA的wifi或4G等网络替换成空,不然切换网络会ID不一样
-    if (index === 0) { return component.value.replace(/\bNetType\/\w+\b/, '')}
-    return component.value
-  })
-  // 生成最终id murmur   
-  const murmur = Fingerprint2.x64hash128(values.join(''), 31)
-  store.commit('SET_MURMUR', murmur)
-})
-
-router.beforeEach((to, from, next) => {
-  /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    document.title = to.meta.title
-  } else {
-    document.title = 'Special Blog'
+	excludes: {
+    language: true,
+    colorDepth: true,
+    deviceMemory: true,
+    pixelRatio: true,
+    availableScreenResolution: true,
+    timezoneOffset: true,
+    timezone: true,
+    sessionStorage: true,
+    localStorage: true,
+    indexedDb: true,
+    addBehavior: true,
+    openDatabase: true,
+    cpuClass: true,
+    doNotTrack: true,
+    plugins: true,
+    canvas: true,
+    webglVendorAndRenderer: true,
+    adBlock: true,
+    hasLiedLanguages: true,
+    hasLiedResolution: true,
+    hasLiedOs: true,
+    hasLiedBrowser: true,
+    touchSupport: true,
+    audio: false,
+    enumerateDevices: true,
+    hardwareConcurrency: true,
   }
-  next()
-})
+}
 
 new Vue({
   router,
   store,
   vuetify,
+  created() {
+    this.generateFingerprint()
+  },
+  methods: {
+    generateFingerprint() {
+      Fingerprint2.get({options},(components) => {
+        const values = components.map((component, index) => {
+          //把微信浏览器里UA的wifi或4G等网络替换成空,不然切换网络会ID不一样
+          if (index === 0) { return component.value.replace(/\bNetType\/\w+\b/, '')}
+          return component.value
+        })
+        // 生成最终id murmur   
+        const murmur = Fingerprint2.x64hash128(values.join(''), 31)
+        store.commit('SET_MURMUR', murmur)
+      })
+    },
+  },
   render: h => h(App)
 }).$mount('#app')
--- src/permission.js
+++ src/permission.js
@@ -1,17 +1,22 @@
 import router from './router'
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requireAuth)) { // 判断该路由是否需要登录权限
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  } else {
+    document.title = 'Special Blog'
+  }
+  // 判断该路由是否需要登录权限
+  if (to.matched.some(record => record.meta.requireAuth)) { 
     const token = localStorage.getItem("token")
-    if (token) { // 判断当前的token是否存在 ； 登录存入的token
-      if (to.path === '/login') {
-      } else {
+    // 判断当前的token是否存在 ； 登录存入的token
+    if (token) { 
+      if (to.path !== '/login') {
         next()
       }
     } else {
-      next({
-        path: '/login'
-      })
+      next({ path: '/login' })
     }
   } else {
     next()
